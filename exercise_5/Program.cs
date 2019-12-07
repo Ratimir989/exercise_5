9d1464b95a10db01472173ed1eefb80c53a32a20
Ratimir <ra0953029364@gmail.com>
2019-12-07 15:37:57 +0200

    Добавьте файлы проекта.

---
+++ exercise_5/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace exercise_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите номер дня недели!");
+            int dayNumber = int.Parse(Console.ReadLine());
+            switch (dayNumber)
+            {
+                case 1:
+                    Console.WriteLine("Понедельник");
+                    break;
+                case 2:
+                    Console.WriteLine("Вторник");
+                    break;
+                case 3:
+                    Console.WriteLine("Среда");
+                    break;
+                case 4:
+                    Console.WriteLine("Четверг");
+                    break;
+                case 5:
+                    Console.WriteLine("Пятница");
+                    break;
+                case 6:
+                    Console.WriteLine("Суббота");
+                    break;
+                case 7:
+                    Console.WriteLine("Воскресенье");
+                    break;
+
+                default:
+                    Console.WriteLine("Нет такого дня");
+                    break;
+            }
+        }
+    }
+}
